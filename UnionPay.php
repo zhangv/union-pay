02e9ec6417a340477aa6e285610e856c1073aa50
zhangv <derekzhangv@gmail.com>
2015-11-16 13:40:57 +0800

    add promoter commission withdraw

--- UnionPay.php
+++ UnionPay.php
@@ -1,6 +1,13 @@
 <?php
-class UnionPay
-{
+class UnionPay {
+	const URL_FRONTTRANS = "https://gateway.95516.com/gateway/api/frontTransReq.do";
+	const URL_BACKTRANS = "https://gateway.95516.com/gateway/api/backTransReq.do";
+	const URL_BATCHTRANS = "https://gateway.95516.com/gateway/api/batchTrans.do";
+	const URL_SINGLEQUERY = "https://gateway.95516.com/gateway/api/queryTrans.do";
+	const URL_FILEQUERY = "https://filedownload.95516.com/";
+	const URL_CARDREQUEST = "https://gateway.95516.com/gateway/api/cardTransReq.do";
+	const URL_APPREQUEST = "https://gateway.95516.com/gateway/api/appTransReq.do";
+
 	/**
 	 * 支付配置
 	 * @var array
@@ -37,6 +44,11 @@ class UnionPay
 </body>
 </html>
 HTML;
+
+	public function __construct($config){
+		$this->config = $config;
+	}
+
 	/**
 	 * 构建自动提交HTML表单
 	 * @return string
@@ -50,29 +62,91 @@ HTML;
 		return sprintf($this->formTemplate, $this->config['frontUrl'], $input);
 	}
 
+	public function pay($orderId,$amt,$desc,$ext){
+		$this->params = [
+			'version' => '5.0.0', //版本号
+			'encoding' => 'UTF-8', //编码方式
+			'certId' => $this->getSignCertId(), //证书ID
+			'signMethod' => '01', //签名方式
+			'txnType' => '01', //交易类型
+			'txnSubType' => '01', //交易子类
+			'bizType' => '000201', //产品类型
+			'channelType' => '08',//渠道类型 08-手机网页支付 07-网关支付
+			'frontUrl' => $this->config['notifyUrl'], //前台通知地址
+			'backUrl' => $this->config['returnUrl'], //后台通知地址
+//			'frontFailUrl' => $config['failUrl'], //失败交易前台跳转地址
+			'accessType' => '0', //接入类型
+			'merId' => $this->config['merId'], //商户代码
+			'orderId' => $orderId, //商户订单号 不能含“-”或“_”
+			'txnTime' => date('YmdHis'), //订单发送时间
+			'txnAmt' => $amt , //交易金额，单位分
+			'currencyCode' => '156', //交易币种
+			'defaultPayType' => '0001',	//默认支付方式
+			//'orderDesc' => $desc, //订单描述，网关支付和wap支付暂时不起作用
+//			'customerIp' => Ip::remote_addr(), //填写持卡人发起交易的IP地址，用于防钓鱼
+			'reqReserved' => $ext,//	商户自定义保留域，交易应答时会原样返回
+//			'orderTimeout' => 86400000 //订单接收超时时间 单位为毫秒，交易发生时，该笔交易在银联全渠道系统中有效的最长时间。当距离交易发送时间超过该时间时，银联全渠道系统不再为该笔交易提供支付服务
+		];
+		return $this->createPostForm();
+	}
+
+	/**
+	 * 支付撤销
+	 */
+	public function payUndo(){}
+
 	/**
 	 * 退款
 	 */
-	public function refund(){
-		$params = array(
+	public function refund($orderId,$origQryId,$refundAmt,$ext){
+		$this->params = [
 			'version' => '5.0.0',		//版本号
 			'encoding' => 'utf-8',		//编码方式
-			'certId' => getSignCertId (),	//证书ID
+			'certId' => $this->getSignCertId(),	//证书ID
 			'signMethod' => '01',		//签名方法
-			'txnType' => '04',		//交易类型
+			'txnType' => '04',		//交易类型 04- 退款
 			'txnSubType' => '00',		//交易子类
 			'bizType' => '000201',		//业务类型
 			'accessType' => '0',		//接入类型
 			'channelType' => '07',		//渠道类型
-			'orderId' => date('YmdHis'),	//商户订单号，重新产生，不同于原消费
-			'merId' => '888888888888888',	//商户代码，请修改为自己的商户号
-			'origQryId' => '201501062125593073808',    //原消费的queryId，可以从查询接口或者通知接口中获取
+			'orderId' => $orderId,	//商户订单号，重新产生，不同于原消费
+			'merId' => $this->config['merId'],	//商户代码，请修改为自己的商户号
+			'origQryId' => $origQryId,    //原消费的queryId，可以从查询接口或者通知接口中获取
 			'txnTime' => date('YmdHis'),	//订单发送时间，重新产生，不同于原消费
-			'txnAmt' => '100',              //交易金额，退货总金额需要小于等于原消费
-			'backUrl' => SDK_BACK_NOTIFY_URL,	   //后台通知地址
-			'reqReserved' =>' 透传信息', //请求方保留域，透传字段，查询、通知、对账文件中均会原样出现
-		);
+			'txnAmt' => $refundAmt,              //交易金额，退货总金额需要小于等于原消费
+			'backUrl' => $this->config['notifyUrl'],	   //后台通知地址
+			'reqReserved' => $ext, //请求方保留域，透传字段，查询、通知、对账文件中均会原样出现
+		];
+		$this->params['signature'] = $this->sign();
+		$result = $this->sendHttpRequest($this->params,self::URL_BACKTRANS);
+		return $result;
 	}
+
+	/**
+	 * 查询交易
+	 */
+	public function query(){}
+	/**
+	 * 文件传输类交易
+	 */
+	public function fileTransfer(){}
+	/**
+	 * 预授权
+	 */
+	public function authDeal(){}
+	/**
+	 * 预授权撤销
+	 */
+	public function authDealUndo(){}
+	/**
+	 * 预授权完成
+	 */
+	public function authDealFinish(){}
+	/**
+	 * 预授权完成撤销
+	 */
+	public function authDealFinishUndo(){}
+
 
 	private function post($url, $data) {
 		$data["sign"] = $this->sign($data);
@@ -90,6 +164,45 @@ HTML;
 	}
 
 	/**
+	 * 后台交易 HttpClient通信
+	 * @param unknown_type $params
+	 * @param unknown_type $url
+	 * @return mixed
+	 */
+	function sendHttpRequest($params, $url) {
+		$opts = $this->getRequestParamString ( $params );
+
+		$ch = curl_init ();
+		curl_setopt ( $ch, CURLOPT_URL, $url );
+		curl_setopt ( $ch, CURLOPT_POST, 1 );
+		curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false);//不验证证书
+		curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false);//不验证HOST
+		curl_setopt ( $ch, CURLOPT_SSLVERSION, 3);
+		curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+			'Content-type:application/x-www-form-urlencoded;charset=UTF-8'
+		) );
+		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $opts );
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+		$html = curl_exec ( $ch );
+		curl_close ( $ch );
+		return $html;
+	}
+
+	/**
+	 * 组装报文
+	 *
+	 * @param unknown_type $params
+	 * @return string
+	 */
+	function getRequestParamString($params) {
+		$params_str = '';
+		foreach ( $params as $key => $value ) {
+			$params_str .= ($key . '=' . (!isset ( $value ) ? '' : urlencode( $value )) . '&');
+		}
+		return substr ( $params_str, 0, strlen ( $params_str ) - 1 );
+	}
+
+	/**
 	 * 验证签名
 	 * 验签规则：
 	 * 除signature域之外的所有项目都必须参加验签
@@ -100,8 +213,7 @@ HTML;
 	 * @throws \Exception
 	 * @return bool
 	 */
-	public function verifySign()
-	{
+	public function verifySign(){
 		$publicKey = $this->getVerifyPublicKey();
 		$verifyArr = $this->filterBeforSign();
 		ksort($verifyArr);
@@ -118,8 +230,7 @@ HTML;
 	 * 取签名证书ID(SN)
 	 * @return string
 	 */
-	public function getSignCertId()
-	{
+	public function getSignCertId(){
 		return $this->getCertIdPfx($this->config['signCertPath']);
 	}
 	/**
@@ -153,8 +264,7 @@ HTML;
 	 * @param array $arr
 	 * @return string
 	 */
-	private function arrayToString($arr)
-	{
+	private function arrayToString($arr){
 		$str = '';
 		foreach($arr as $key => $value) {
 			$str .= $key.'='.$value.'&';
@@ -167,8 +277,7 @@ HTML;
 	 *
 	 * @return array
 	 */
-	private function filterBeforSign()
-	{
+	private function filterBeforSign(){
 		$tmp = $this->params;
 		unset($tmp['signature']);
 		return $tmp;
@@ -178,8 +287,7 @@ HTML;
 	 * @param string $data 待签名数据
 	 * @return mixed
 	 */
-	private function rsaSign($data)
-	{
+	private function rsaSign($data){
 		$privatekey = $this->getSignPrivateKey();
 		$result = openssl_sign($data, $signature, $privatekey);
 		if($result) {
@@ -191,8 +299,7 @@ HTML;
 	 * 取.pfx格式证书ID(SN)
 	 * @return string
 	 */
-	private function getCertIdPfx($path)
-	{
+	private function getCertIdPfx($path){
 		$pkcs12certdata = file_get_contents($path);
 		openssl_pkcs12_read($pkcs12certdata, $certs, $this->config['signCertPwd']);
 		$x509data = $certs['cert'];
@@ -204,8 +311,7 @@ HTML;
 	 * 取.cer格式证书ID(SN)
 	 * @return string
 	 */
-	private function getCertIdCer($path)
-	{
+	private function getCertIdCer($path){
 		$x509data = file_get_contents($path);
 		openssl_x509_read($x509data);
 		$certdata = openssl_x509_parse($x509data);
@@ -215,8 +321,7 @@ HTML;
 	 * 取签名证书私钥
 	 * @return resource
 	 */
-	private function getSignPrivateKey()
-	{
+	private function getSignPrivateKey(){
 		$pkcs12 = file_get_contents($this->config['signCertPath']);
 		openssl_pkcs12_read($pkcs12, $certs, $this->config['signCertPwd']);
 		return $certs['pkey'];
@@ -226,10 +331,9 @@ HTML;
 	 * @throws \InvalidArgumentException
 	 * @return string
 	 */
-	private function getVerifyPublicKey()
-	{
+	private function getVerifyPublicKey(){
 		//先判断配置的验签证书是否银联返回指定的证书是否一致
-		if($this->getCertIdCer($this->config['verifyCertPath']) != $this->params['certId']) {
+		if($this->getCertIdCer($this->config['verifyCertPath']) != $this->getSignCertId()) {
 			throw new \InvalidArgumentException('Verify sign cert is incorrect');
 		}
 		return file_get_contents($this->config['verifyCertPath']);
